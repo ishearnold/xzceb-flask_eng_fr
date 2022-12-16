d7004bd198eecbc6671f19447ad77252bba7fe80
ishearnold <ishearnold>
2022-12-16 01:26:50 -0500

    Temp changes

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,13 @@
+import unittest
+from translator import frenchToEnglish, englishToFrench
+
+class TestTranslator(unittest.TestCase):
+    def test_eng(self)
+        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
+    
+    def test_french(self)
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+
+if __name__=='__main__':
+    unittest.main()
+    
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,9 +9,19 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('lRSVePI2jCdQxO4RP4Kz4A5S7lFFHEnHLHNhvIgHx9WQ')
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+language_translator.set_service_url(url)
+#language_translator.set_disable_ssl_verification(True)
 
+def englishToFrench(englishText):
+    #language_translator.set_service_url(url)
+    frenchText=language_translator.translate(text=englishText,model_id='en-fr').get_result()
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    #language_translator.set_service_url(url)
+    englishText=language_translator.translate(text=frenchText,model_id='fr-en').get_result()
+    return englishText
+
+#print(englishToFrench('Hello'))
