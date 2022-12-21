4d4cf55c645090c3a09ca9fd7e20b7531ac34ba8
ishearnold <chiswanda@hotmail.com>
2022-12-20 23:57:41 -0500

    Temp Changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,11 +6,13 @@ class TestTranslator(unittest.TestCase):
         self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
         self.assertEqual(frenchToEnglish(None),None)
         self.assertEqual(frenchToEnglish(''),'')
+        self.assertNotEqual(frenchToEnglish('Rouge'), 'Green')
 
     def test_2(self):
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
         self.assertEqual(englishToFrench(None),None)
         self.assertEqual(englishToFrench(''),'')
+        self.assertNotEqual(englishToFrench('Green'), 'Rouge')
 
 if __name__=='__main__':
     unittest.main()
