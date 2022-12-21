54db7af32395583a117157d8a4cce6cd406192a1
ishearnold <chiswanda@hotmail.com>
2022-12-20 23:52:27 -0500

    Temp Changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
+'''Module providing ability to run python script'''
+import os
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,17 +15,22 @@ language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=a
 language_translator.set_service_url(url)
 language_translator.set_disable_ssl_verification(True)
 
-def englishToFrench(self, englishText):
+def englishToFrench(englishText):
+    '''
+    Translate english text to french. Return french text
+    '''
     if englishText =='' or englishText is None:
         return ''
     translation=language_translator.translate(text=englishText,model_id='en-fr').get_result()
     frenchText=translation['translations'][0]['translation']
     return frenchText
 
-def frenchToEnglish(self, frenchText):
+def frenchToEnglish(frenchText):
+    '''
+    Translate french text to english. Return french text
+    '''
     if frenchText =='' or frenchText is None:
         return ''
     translation=language_translator.translate(text=frenchText,model_id='fr-en').get_result()
     englishText=translation['translations'][0]['translation']
     return englishText
-
